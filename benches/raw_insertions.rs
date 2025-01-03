b2c34407e4da9daec2860a2f556352f4e248c985
Suraj Choudhary <suraj@aeriuslabs.org>
2025-01-03 13:01:03 +0530

    fix: use field array elements for price, time, volume in benches

--- benches/raw_insertions.rs
+++ benches/raw_insertions.rs
@@ -1,15 +1,21 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::Rng;
 use std::hint::black_box;
-use stwo_prover::core::fields::m31::M31;
 use vex_prover::imt::order::Order;
 use vex_prover::imt::IndexedMerkleTree;
+use vex_prover::types::{Price, Time, Volume};
 
 fn insert_batch(n: u32) {
     let mut imt = IndexedMerkleTree::new();
     let mut rng = rand::thread_rng();
     let orders = (0..n)
-        .map(|i| Order::new(M31(i), M31(rng.gen_range(21352332..33333333)), M31(i + 2)))
+        .map(|i| {
+            Order::new(
+                Volume::from_u64(rng.gen()),
+                Price::from_u64(rng.gen()),
+                Time::from_u64((i + 2) as u64),
+            )
+        })
         .collect::<Vec<_>>();
     for order in orders {
         imt.insert(order);
