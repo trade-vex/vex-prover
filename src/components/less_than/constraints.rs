0dc9aae00890ccbb3f666db750a0f17ac132e418
Suraj Choudhary <suraj@aeriuslabs.org>
2025-03-02 14:57:09 +0530

    feat: less than component

---
+++ src/components/less_than/constraints.rs
@@ -0,0 +1,124 @@
+use itertools::{chain, izip};
+use num_traits::{One, Zero};
+use stwo_prover::constraint_framework::{EvalAtRow, FrameworkEval, RelationEntry};
+
+use crate::components::{bytes::LessThanU8Elements, Claim};
+
+use super::{LessThanColumn, LessThanElements, LessThanOp};
+
+#[derive(Clone)]
+pub struct LessThanEval {
+    pub claim: Claim<LessThanColumn>,
+    pub less_than_u8_elements: LessThanU8Elements,
+    pub less_than_elements: LessThanElements,
+}
+
+/// This implementation of the `FrameworkEval` trait for `LessThanEval` provides methods to evaluate
+/// constraints on less than operations. The primary purpose of this
+/// implementation is to evaluate specific constraints related to less than operations used by other components.
+///
+/// # Constaint Evaluation
+///
+/// - `evaluate<E: EvalAtRow>(&self, mut eval: E) -> E`:
+///   It retrieves the LessThanOp for a particular row, each row represents a single less than operation.
+///
+///   The method follows these steps:
+///   1. Retrieve LessThanOp for the row
+///   2. IsReal must be a boolean. it is true if the operation is from non-padded row.
+///   3. Each flag must be a boolean. The sum of flags must be either 0 or 1.
+///      Flag represents an array of booleans, has one elment set to true if a_byte is not equal to b_byte.
+///   4. The sum of flags must be either 0 or 1. 0 if a is equal to b, 1 if a is not equal to b.
+///   5. Loop through each byte of a and b, and add constraints:
+///        if inequality is not visited yet then a_byte must be equal to b_byte and record the first byte where a is not equal to b.
+///   6. Add constraints for a_comparison_byte and b_comparison_byte.
+///   7. The Result of the Operation op.c must be a boolean.
+///   8. The Result of the Operation op.c must be 1 if a_comparision_byte is less than b_comparison_byte
+///      which is checked by looking up the result of the comparison bytes from the less_than_u8_elements
+///      from the PreProcessedBytes Table.
+///        - Multiplicity of the relation is the is_real flag.
+///        - Values are a_comparision_byte, b_comparison_byte, and c.
+///   9. Yield the Results by adding the values to the less_than_elements.   
+///        - Multiplicity of the relation is the negation of is_real flag.
+///        - Values are a, b, and c.
+///   10. Finalize the evaluation by calling `eval.finalize_logup_in_pairs()`.
+impl FrameworkEval for LessThanEval {
+    fn log_size(&self) -> u32 {
+        self.claim.log_size
+    }
+    fn max_constraint_log_degree_bound(&self) -> u32 {
+        self.claim.log_size + 1
+    }
+    fn evaluate<E: EvalAtRow>(&self, mut eval: E) -> E {
+        let op = LessThanOp::<E::F>::from_eval(&mut eval);
+
+        // is_real must be a boolean
+        eval.add_constraint(op.is_real.clone() * (op.is_real.clone() - E::F::one()));
+
+        // Each flag must be boolean
+        let mut sum_flags = E::F::zero();
+        for flag in op.flags.iter() {
+            eval.add_constraint(flag.clone() * (E::F::one() - flag.clone()));
+            sum_flags = sum_flags.clone() + flag.clone();
+        }
+        // sum must be either 0 or 1
+        // 0 if a is equal to b
+        // 1 if a is not equal to b
+        eval.add_constraint(sum_flags.clone() * (E::F::one() - sum_flags.clone()));
+
+        // a_comparison_byte and b_comparison_byte must be equal to the first bytes where a < b
+        let mut is_inequality_visited = E::F::zero();
+        let mut a_comparison_byte = E::F::zero();
+        let mut b_comparison_byte = E::F::zero();
+        for (a_byte, b_byte, flag) in izip!(
+            op.a.clone().into_iter().rev(),
+            op.b.clone().into_iter().rev(),
+            op.flags.into_iter().rev()
+        ) {
+            is_inequality_visited = is_inequality_visited.clone() + flag.clone();
+            a_comparison_byte = a_comparison_byte.clone() + a_byte.clone() * flag.clone();
+            b_comparison_byte = b_comparison_byte.clone() + b_byte.clone() * flag;
+
+            // if inequality is not visited yet then a_byte must be equal to b_byte
+            eval.add_constraint(
+                (E::F::one() - is_inequality_visited.clone()) * (a_byte.clone() - b_byte.clone()),
+            );
+        }
+        eval.add_constraint(op.a_comparison_byte.clone() - a_comparison_byte.clone());
+        eval.add_constraint(op.b_comparison_byte.clone() - b_comparison_byte.clone());
+
+        // c must be a boolean
+        eval.add_constraint(op.c.clone() * (op.c.clone() - E::F::one()));
+
+        // if a and b are equal i.e sum_flags is 0
+        // is_inequality_visited must be 0
+        // a_comparison_byte must be 0
+        // b_comparison_byte must be 0
+        // c must be 0
+        eval.add_constraint((E::F::one() - sum_flags.clone()) * is_inequality_visited.clone());
+        eval.add_constraint((E::F::one() - sum_flags.clone()) * a_comparison_byte.clone());
+        eval.add_constraint((E::F::one() - sum_flags.clone()) * b_comparison_byte.clone());
+        eval.add_constraint((E::F::one() - sum_flags.clone()) * op.c.clone());
+
+        // c must be 1 if a_comparision_byte is less than b_comparison_byte
+        // c must be 0 if a_comparision_byte is greater than b_comparison_byte
+        // Look Up if the c has been set correctly and "use" the result of the comparison bytes
+        eval.add_to_relation(RelationEntry::new(
+            &self.less_than_u8_elements,
+            E::EF::from(op.is_real.clone()),
+            &[op.a_comparison_byte, op.b_comparison_byte, op.c.clone()],
+        ));
+
+        // Yield the Results
+        let values: Vec<E::F> =
+            chain!(op.a.into_iter(), op.b.into_iter(), std::iter::once(op.c)).collect();
+
+        eval.add_to_relation(RelationEntry::new(
+            &self.less_than_elements,
+            -E::EF::from(op.is_real.clone()),
+            &values,
+        ));
+
+        eval.finalize_logup_in_pairs();
+        eval
+    }
+}
---
+++ src/components/less_than/mod.rs
@@ -0,0 +1,150 @@
+use crate::types::N_U64_LIMBS;
+use std::array;
+use stwo_prover::{
+    constraint_framework::EvalAtRow,
+    core::fields::{m31::BaseField, secure_column::SECURE_EXTENSION_DEGREE},
+    relation,
+};
+
+use super::TraceSize;
+mod constraints;
+mod trace;
+
+pub use trace::{interaction_trace, preprocessed_trace, trace};
+
+/// Less Than Operations
+pub type LessThanOperations = Vec<[BaseField; LessThanColumn::MAIN_COLS]>;
+
+/// LessThanOp represents the LessThan operation between two uint represented as N u8 limbs.
+/// Example for BaseField = M31 and uint64_t: N = 8
+#[derive(Debug)]
+pub struct LessThanOp<F> {
+    // first operand
+    a: [F; N_U64_LIMBS],
+    // second operand
+    b: [F; N_U64_LIMBS],
+    // result of the comparison
+    c: F,
+    // flag is 1 for the most significant byte where a[i] < b[i]
+    flags: [F; N_U64_LIMBS],
+    // First byte of the first operand where a[i] < b[i] from the most significant byte
+    a_comparison_byte: F,
+    // First byte of the second operand where a[i] < b[i] from the most significant byte
+    b_comparison_byte: F,
+    // is real flag to check if the operation is not among the dummy padded operations
+    is_real: F,
+}
+
+impl<F> LessThanOp<F> {
+    /// from_eval returns a LessThanOp instance from a given EvalAtRow instance
+    fn from_eval<E: EvalAtRow>(eval: &mut E) -> LessThanOp<E::F> {
+        let a = array::from_fn(|_| eval.next_trace_mask());
+        let b = array::from_fn(|_| eval.next_trace_mask());
+        let c = eval.next_trace_mask();
+        let flags = array::from_fn(|_| eval.next_trace_mask());
+        let a_comparison_byte = eval.next_trace_mask();
+        let b_comparison_byte = eval.next_trace_mask();
+        let is_real = eval.next_trace_mask();
+        LessThanOp {
+            a,
+            b,
+            c,
+            flags,
+            a_comparison_byte,
+            b_comparison_byte,
+            is_real,
+        }
+    }
+}
+
+/// LessThanColumn represents a Column for the starting limb of a field in LessThanOp
+#[derive(Debug, Clone)]
+pub struct LessThanColumn;
+
+impl LessThanColumn {
+    pub const A: usize = 0;
+    pub const B: usize = Self::A + N_U64_LIMBS;
+    pub const C: usize = Self::B + N_U64_LIMBS;
+    pub const FLAGS: usize = Self::C + 1;
+    pub const A_COMPARISON_BYTE: usize = Self::FLAGS + N_U64_LIMBS;
+    pub const B_COMPARISON_BYTE: usize = Self::A_COMPARISON_BYTE + 1;
+    pub const IS_REAL: usize = Self::B_COMPARISON_BYTE + 1;
+}
+
+impl TraceSize for LessThanColumn {
+    // last field's index + offset
+    const MAIN_COLS: usize = Self::IS_REAL + 1;
+    // 1 Interaction Column for LessThanU8 check
+    // 1 Interaction Column for yielding the result
+    const INTERACTION_COLS: usize = 2 * SECURE_EXTENSION_DEGREE;
+}
+
+relation!(LessThanElements, 18);
+
+#[cfg(test)]
+mod tests {
+    use constraints::LessThanEval;
+    use rand::Rng;
+    use stwo_prover::{
+        constraint_framework::{assert_constraints, FrameworkEval},
+        core::{channel::Blake2sChannel, pcs::TreeVec, poly::circle::CanonicCoset},
+    };
+    use trace::{interaction_trace, preprocessed_trace, trace};
+    use tracing::{span, Level};
+
+    use crate::{
+        components::bytes::LessThanU8Elements, executor::record::ExecutionTrace, types::Price,
+    };
+
+    use super::*;
+
+    #[test_log::test]
+    fn test_less_than_table() {
+        // Execution Record
+        let span = span!(Level::INFO, "Generating Execution Record").entered();
+        let mut record = ExecutionTrace::new();
+        let n = 1242132;
+        let mut rng = rand::thread_rng();
+        for _ in 0..n {
+            let a: Price<BaseField> = Price::from_u64(rng.gen());
+            let b: Price<BaseField> = Price::from_u64(rng.gen());
+            record.add_less_than_event(a.to_felts(), b.to_felts());
+        }
+        let log_size = (record.less_than_operations.len() - 1).ilog2() + 1;
+        span.exit();
+
+        // Fiat Shamir Channel
+        let mut channel = Blake2sChannel::default();
+
+        // Relation Elements
+        let less_than_u8_elements = LessThanU8Elements::draw(&mut channel);
+        let less_than_elements = LessThanElements::draw(&mut channel);
+
+        // Trace Generation
+        let span = span!(Level::INFO, "Trace Generation").entered();
+        let constant_trace = preprocessed_trace(log_size);
+        let (trace, claim) = trace(record.less_than_operations);
+        let (interaction_trace, interaction_claim) =
+            interaction_trace(&trace, &less_than_u8_elements, &less_than_elements);
+        span.exit();
+        let trace = TreeVec::new(vec![constant_trace, trace, interaction_trace]);
+        let trace_polys = TreeVec::<Vec<_>>::map_cols(trace, |c| c.interpolate());
+
+        let component = LessThanEval {
+            less_than_u8_elements,
+            less_than_elements,
+            claim,
+        };
+
+        // panics if the constraints are not satisfied
+        let _span = span!(Level::INFO, "Constraint Assertion").entered();
+        assert_constraints(
+            &trace_polys,
+            CanonicCoset::new(log_size),
+            |eval| {
+                component.evaluate(eval);
+            },
+            interaction_claim.claimed_sum,
+        )
+    }
+}
