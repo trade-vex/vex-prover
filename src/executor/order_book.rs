3795bf9b1b6a56bee557907dab18af350b685b42
Suraj Choudhary <suraj@aeriuslabs.org>
2025-03-12 16:52:46 +0530

    fix: less than events

--- src/executor/order_book.rs
+++ src/executor/order_book.rs
@@ -168,7 +168,7 @@ impl OrderBook {
             // best(buy) >= best(sell)
             self.trace
                 .borrow_mut()
-                .add_less_than_event(order.price().to_felts(), match_leaf.price().to_felts())?;
+                .add_less_than_event(match_leaf.price().to_felts(), order.price().to_felts())?;
             if order.volume == Volume::zero() {
                 let proof = self.buy_imt.match_order()?;
                 self.finalize_match(proof, true)?;
@@ -208,7 +208,7 @@ impl OrderBook {
 
             self.trace
                 .borrow_mut()
-                .add_less_than_event(match_leaf.price().to_felts(), order.price().to_felts())?;
+                .add_less_than_event(order.price().to_felts(),match_leaf.price().to_felts())?;
             if order.volume == Volume::zero() {
                 let proof = self.sell_imt.match_order()?;
                 self.finalize_match(proof, true)?;
