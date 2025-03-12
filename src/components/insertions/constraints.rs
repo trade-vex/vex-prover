7b575081059dfa9cc3ad43babb266dd0c4d3954a
Suraj Choudhary <suraj@aeriuslabs.org>
2025-03-12 10:17:13 +0530

    chore: add constraints utils for common constraints

--- src/components/insertions/constraints.rs
+++ src/components/insertions/constraints.rs
@@ -1,4 +1,5 @@
 use crate::{
+    components::constraints_utils::eval_merkle_proof,
     executor::{flatten_single, instruction::IMTOperation},
     imt::{
         leaf::Leaf,
@@ -19,11 +20,11 @@ use crate::{
     },
     executor::instruction::{Instruction, InstructionElements},
     flatten,
-    hash::{N_HASH, N_STATE},
+    hash::N_HASH,
     imt::{
         leaf::LeafColumn,
         side::{OrderSide, Side},
-        IndexBits, LeafFelts, MerklePath, MerkleProof, MERKLE_HEIGHT, N_LEAF_FELTS, N_U64_FELTS,
+        MERKLE_HEIGHT, N_LEAF_FELTS, N_U64_FELTS,
     },
 };
 
@@ -379,44 +380,5 @@ impl<S: OrderSide> FrameworkEval for InsertionsEval<S> {
         ));
         eval.finalize_logup();
         eval
-    }
-}
-
-fn eval_merkle_proof<E: EvalAtRow>(
-    eval: &mut E,
-    proof: MerkleProof<E::F>,
-    path: MerklePath<E::F>,
-    leaf: LeafFelts<E::F>,
-    index_bits: IndexBits<E::F>,
-    poseidon_elements: &PoseidonElements,
-    mult: E::EF,
-) {
-    // evaluate leaf hash
-    let leaf: Vec<E::F> = leaf[0..N_STATE].try_into().unwrap();
-    eval.add_to_relation(RelationEntry::new(
-        poseidon_elements,
-        mult.clone(),
-        &chain!(leaf.iter().cloned(), path[0].clone().into_iter()).collect::<Vec<_>>(),
-    ));
-
-    let mut curr = path[0].clone();
-    for (i, (sibling, hash)) in proof.iter().zip(path.iter().skip(1)).enumerate() {
-        let index_bit = index_bits[i].clone();
-        let first: [E::F; N_HASH] = array::from_fn(|j| {
-            index_bit.clone() * sibling[j].clone()
-                + (E::F::one() - index_bit.clone()) * curr[j].clone()
-        });
-        let second: [E::F; N_HASH] = array::from_fn(|j| {
-            index_bit.clone() * curr[j].clone()
-                + (E::F::one() - index_bit.clone()) * sibling[j].clone()
-        });
-        let values: Vec<E::F> = chain!(
-            first.into_iter(),
-            second.into_iter(),
-            hash.clone().into_iter()
-        )
-        .collect();
-        eval.add_to_relation(RelationEntry::new(poseidon_elements, mult.clone(), &values));
-        curr = hash.clone();
     }
 }
