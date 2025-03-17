2fec7123857d73b46b42e82005304b235b442010
Suraj Choudhary <suraj@aeriuslabs.org>
2025-03-17 12:27:13 +0530

    chore: add error for zero volume fill in partial match

--- src/imt/error.rs
+++ src/imt/error.rs
@@ -11,6 +11,7 @@ pub enum IMTError {
     InsufficientVolumeToFill(u64, u64),
     InvalidU8Pair(u32, u32),
     InvalidOrder,
+    NotAPartialMatch,
 }
 
 impl fmt::Display for IMTError {
@@ -27,6 +28,7 @@ impl fmt::Display for IMTError {
             }
             IMTError::InvalidU8Pair(a, b) => write!(f, "Invalid u8 pair: {} {}", a, b),
             IMTError::InvalidOrder => write!(f, "Volume, Price, Time cannot be zero"),
+            IMTError::NotAPartialMatch => write!(f, "Not a partial match"),
         }
     }
 }
