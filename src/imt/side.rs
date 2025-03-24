caf9cd6803aaa77128581c66121f54db651f840c
Suraj Choudhary <suraj@aeriuslabs.org>
2025-03-24 16:52:49 +0530

    chore: refactor Side::name()

--- src/imt/side.rs
+++ src/imt/side.rs
@@ -32,10 +32,7 @@ pub trait OrderSide: 'static + Copy + Send + Sync + Clone + Copy + Debug {
     fn op_code(op: IMTOperation) -> BaseField;
     /// get the name of the side
     fn name() -> &'static str {
-        match Self::side() {
-            Side::Buy => "Buy",
-            Side::Sell => "Sell",
-        }
+        Self::NAME
     }
 }
 
